5ef780589405fc237e3bce40eb7a91bc2c793848
theWolf1198 <kavel.barua@gmail.com>
2017-12-27 00:17:34 +0530

    Changed the Current

--- app/src/main/java/com/example/kavel/location_news/Menu_activity.java
+++ app/src/main/java/com/example/kavel/location_news/Menu_activity.java
@@ -1,8 +1,10 @@
 package com.example.kavel.location_news;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.location.LocationManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -14,15 +16,22 @@ import android.view.View;
 
 public class Menu_activity extends AppCompatActivity {
 
+    LocationManager locManager;
+    boolean network_enabled;
+    Location location;
+    Intent intent;
+
     public void switchToMapsActivity(View view){
 
-        Intent intent = new Intent(getApplicationContext(), MapsActivity_main.class);
+        intent = new Intent(getApplicationContext(), MapsActivity_main.class);
+        intent.putExtra("location", location);
         startActivity(intent);
     }
 
     public void switchToLocalNewsActivity(View view){
 
-        Intent intent = new Intent(getApplicationContext(), currentLocationNews.class);
+        intent = new Intent(getApplicationContext(), currentLocationNews.class);
+        intent.putExtra("location", location);
         startActivity(intent);
     }
 
@@ -34,20 +43,52 @@ public class Menu_activity extends AppCompatActivity {
 
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
 
-                Log.i("Info", "Application Started");
+                if (network_enabled) {
+
+                    location = locManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+                    if (location != null) {
+
+                        Log.i("CurrentNetworkLocation.", location.toString());
+
+
+                    }
+
+                }
+
             }
         }
     }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_activity);
 
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+        locManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        network_enabled = locManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         }
+
+        else {
+
+            if(network_enabled){
+
+                location = locManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+                if(location!=null){
+
+                    Log.i("CurrentNetworkLocation.", location.toString());
+                }
+            }
+
+        }
+
 
     }
 }
