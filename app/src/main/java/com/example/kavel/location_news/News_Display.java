036df48c0cb7660076b69bfa4e5cbc13ee1be813
theWolf1198 <kavel.barua@gmail.com>
2017-12-28 04:09:13 +0530

    News Display now working for a dummy location.

--- app/src/main/java/com/example/kavel/location_news/News_Display.java
+++ app/src/main/java/com/example/kavel/location_news/News_Display.java
@@ -1,12 +1,17 @@
 package com.example.kavel.location_news;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -31,7 +36,11 @@ public class News_Display extends AppCompatActivity {
     String locality;
     String subAdminArea;
     String AdminArea;
+    ListView newsDisplay;
     ArrayList<String> titles = new ArrayList();
+    ArrayList<String> author = new ArrayList();
+    ArrayList<String> urlofNewsSource = new ArrayList();
+    int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,23 +49,19 @@ public class News_Display extends AppCompatActivity {
 
         DownloadTask task = new DownloadTask();
         task.execute("https://newsapi.org/v2/everything?q=jorhat&apiKey=0a3f06e922a84e07b9b99fcbca201d58");
+        newsDisplay = findViewById(R.id.listView);
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
         locality = bundle.getString("locality");
         subAdminArea = bundle.getString("subAdminArea");
         AdminArea = bundle.getString("AdminArea");
-        ListView newsDisplay = findViewById(R.id.listView);
-        CustomAdapter customAdapter = new CustomAdapter();
-        newsDisplay.setAdapter(customAdapter);
 
     }
 
-    class CustomAdapter extends BaseAdapter{
-
-
+    public class CustomAdapter extends BaseAdapter{
         @Override
         public int getCount() {
-            return 100;
+            return titles.size();
         }
 
         @Override
@@ -73,16 +78,30 @@ public class News_Display extends AppCompatActivity {
         public View getView(int i, View view, ViewGroup viewGroup) {
 
             view = getLayoutInflater().inflate(R.layout.customlayout, null);
+            ImageView newsImage = findViewById(R.id.imageView);
+            Log.i("i", Integer.toString(i));
+            TextView headline = (TextView)view.findViewById(R.id.headlineView);
+            TextView authorView = (TextView)view.findViewById(R.id.authorView);
 
-            ImageView imageView = (ImageView)view.findViewById(R.id.imageView);
-            TextView textView = (TextView)view.findViewById(R.id.textView);
-            TextView textView2 = (TextView)view.findViewById(R.id.textView2);
+            if(titles.get(i) != null) {
 
-            imageView.setBackgroundResource(R.drawable.icon1);
-            textView.setText(locality);
-            textView2.setText(AdminArea);
+                headline.setText(titles.get(i));
+            }
+            else{
+
+                Log.i("title", "null");
+
+            }
+
+            if(author.get(i) != null) {
+
+                authorView.setText(author.get(i));
+            }
+            else
+            {
+                Log.i("title", "null");
+            }
             return view;
-
         }
     }
     public class DownloadTask extends AsyncTask<String, Void, String> {
@@ -132,16 +151,25 @@ public class News_Display extends AppCompatActivity {
                 JSONArray arr = new JSONArray(articles);
                 for(int i = 0; i < arr.length(); i++){
 
+                    count = i;
                     JSONObject jsonPart = arr.getJSONObject(i);
                     titles.add(jsonPart.getString("title"));
+                    author.add(jsonPart.getString("author"));
+                    urlofNewsSource.add(jsonPart.getString("url"));
+                    Log.i("titles", titles.get(count));
+                    Log.i("author", author.get(count));
+                    Log.i("urlofNewsSource", urlofNewsSource.get(count));
 
                 }
 
+                CustomAdapter customAdapter = new CustomAdapter();
+                newsDisplay.setAdapter(customAdapter);
+
             } catch (JSONException e) {
                 e.printStackTrace();
+                Log.i("Error occured", "Error");
             }
 
-            Log.i("title5", titles.get(5));
 
         }
     }
